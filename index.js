cb87dbafd926e05f8c6a4f9e1011ba5f673c49c0
Cesar Murillo <namiayumis@gmail.com>
2020-01-04 14:56:24 +0900

    install GraphiQL

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from "express";
+//graphql
+import graphqlHTTP from "express-graphql";
+import schema from "./schema";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Todo listo");
 });
+
+//el resolver
+const root = { hola: () => "Hola Mundo desde GraphQL" };
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    //que schema va a utilizar
+    schema,
+    // el resolver se pasa como rootValue
+    rootValue: root,
+    // utilizar graphiql
+    graphiql: true
+  })
+);
 
 app.listen(8000, () => console.log("servidor esta funcionando"));
