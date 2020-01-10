0f83e44e53316550e7a1807b56538549348a1a35
Cesar Murillo <namiayumis@gmail.com>
2020-01-10 17:56:48 +0900

    creando nuevos clientes

--- cliente/src/componentes/NuevoCliente.js
+++ cliente/src/componentes/NuevoCliente.js
@@ -2,9 +2,66 @@ import React from "react";
 
 const NuevoCliente = () => {
   return (
-    <div>
+    <>
       <h2 className="text-center">Nuevo Cliente</h2>
-    </div>
+      <div className="row justify-content-center">
+        <form className="col-md-8 m-3">
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label>Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nombre"
+              />
+            </div>
+            <div className="form-group col-md-6">
+              <label>Apellido</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Apellido"
+              />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label>Empresa</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Empresa"
+              />
+            </div>
+            <div className="form-group col-md-6">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label>Edad</label>
+              <input type="text" className="form-control" placeholder="Edad" />
+            </div>
+            <div className="form-group col-md-6">
+              <label>Tipo Cliente</label>
+              <select className="form-control">
+                <option value="">Elegir...</option>
+                <option value="PREMIUM">PREMIUM</option>
+                <option value="BASICO">BÁSICO</option>
+              </select>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-success float-right">
+            Guardar Cambios
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
