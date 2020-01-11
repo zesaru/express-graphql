4209aba0e2d8e5ec8631474f3f92b4195f3d88a7
Cesar Murillo <namiayumis@gmail.com>
2020-01-11 18:57:46 +0900

    Leyendo los Datos del Formulario

--- cliente/src/componentes/NuevoCliente.js
+++ cliente/src/componentes/NuevoCliente.js
@@ -1,68 +1,147 @@
-import React from "react";
+import React, { Component } from "react";
 
-const NuevoCliente = () => {
-  return (
-    <>
-      <h2 className="text-center">Nuevo Cliente</h2>
-      <div className="row justify-content-center">
-        <form className="col-md-8 m-3">
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label>Nombre</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Nombre"
-              />
+class NuevoCliente extends Component {
+  state = {
+    cliente: {
+      nombre: "",
+      apellido: "",
+      empresa: "",
+      edad: "email",
+      tipo: ""
+    }
+  };
+  render() {
+    return (
+      <>
+        <h2 className="text-center">Nuevo Cliente</h2>
+        <div className="row justify-content-center">
+          <form
+            className="col-md-8 m-3"
+            onSubmit={e => {
+              e.preventDefault();
+
+              const {
+                nombre,
+                apellido,
+                empresa,
+                edad,
+                tipo,
+                email
+              } = this.state.cliente;
+              const input = {
+                nombre,
+                apellido,
+                empresa,
+                edad: Number.edad,
+                tipo,
+                email
+              };
+
+              console.log(input);
+            }}
+          >
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label>Nombre</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre"
+                  onChange={e => {
+                    this.setState({
+                      cliente: {
+                        ...this.state.cliente,
+                        nombre: e.target.value
+                      }
+                    });
+                  }}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label>Apellido</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Apellido"
+                  onChange={e => {
+                    this.setState({
+                      cliente: {
+                        ...this.state.cliente,
+                        apellido: e.target.value
+                      }
+                    });
+                  }}
+                />
+              </div>
             </div>
-            <div className="form-group col-md-6">
-              <label>Apellido</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Apellido"
-              />
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label>Empresa</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Empresa"
+                  onChange={e => {
+                    this.setState({
+                      cliente: {
+                        ...this.state.cliente,
+                        empresa: e.target.value
+                      }
+                    });
+                  }}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label>Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  onChange={e => {
+                    this.setState({
+                      cliente: {
+                        ...this.state.cliente,
+                        email: e.target.value
+                      }
+                    });
+                  }}
+                />
+              </div>
             </div>
-          </div>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label>Empresa</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Empresa"
-              />
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label>Edad</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Edad"
+                  onChange={e => {
+                    this.setState({
+                      cliente: {
+                        ...this.state.cliente,
+                        edad: e.target.value
+                      }
+                    });
+                  }}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label>Tipo Cliente</label>
+                <select className="form-control">
+                  <option value="">Elegir...</option>
+                  <option value="PREMIUM">PREMIUM</option>
+                  <option value="BASICO">BÁSICO</option>
+                </select>
+              </div>
             </div>
-            <div className="form-group col-md-6">
-              <label>Email</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email"
-              />
-            </div>
-          </div>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label>Edad</label>
-              <input type="text" className="form-control" placeholder="Edad" />
-            </div>
-            <div className="form-group col-md-6">
-              <label>Tipo Cliente</label>
-              <select className="form-control">
-                <option value="">Elegir...</option>
-                <option value="PREMIUM">PREMIUM</option>
-                <option value="BASICO">BÁSICO</option>
-              </select>
-            </div>
-          </div>
-          <button type="submit" className="btn btn-success float-right">
-            Guardar Cambios
-          </button>
-        </form>
-      </div>
-    </>
-  );
-};
+            <button type="submit" className="btn btn-success float-right">
+              Guardar Cambios
+            </button>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
 
 export default NuevoCliente;
