044879c734499304c57e1576316f5fcd0d1df604
Cesar Murillo <namiayumis@gmail.com>
2020-01-12 19:55:30 +0900

    Realizando el Query para obtener los datos del cliente

--- cliente/src/componentes/EditarCliente.js
+++ cliente/src/componentes/EditarCliente.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { CLIENTE_QUERY } from "../queries";
 import { Query } from "react-apollo";
 
+import FormularioeEditarCliente from "./FormularioEditarCliente";
+
 class EditarCliente extends Component {
   state = {};
   render() {
@@ -11,14 +13,16 @@ class EditarCliente extends Component {
       <>
         <h2 className="text-center">Editar Cliente</h2>
 
-        <Query query={CLIENTE_QUERY} variables={{ id }}>
-          {({ loading, error, data }) => {
-            if (loading) return "Cargando...";
-            if (error) return `Error! ${error.message}`;
-
-            console.log(data);
-          }}
-        </Query>
+        <div className="row justify-content-center">
+          <Query query={CLIENTE_QUERY} variables={{ id }}>
+            {({ loading, error, data }) => {
+              if (loading) return "Cargando...";
+              if (error) return `Error! ${error.message}`;
+              console.log(data);
+              return <FormularioeEditarCliente />;
+            }}
+          </Query>
+        </div>
       </>
     );
   }
