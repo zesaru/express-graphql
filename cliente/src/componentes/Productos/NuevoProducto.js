4d9a1dc4320371ad14a5c43ccaf70861196f037c
Cesar Murillo <namiayumis@gmail.com>
2020-01-16 22:56:12 +0900

    Leyendo los Datos desde el Formulario

--- cliente/src/componentes/Productos/NuevoProducto.js
+++ cliente/src/componentes/Productos/NuevoProducto.js
@@ -1,8 +1,65 @@
 import React, { Component } from "react";
 
 class NuevoProducto extends Component {
+  state = {
+    nombre: "",
+    precio: "",
+    stock: ""
+  };
+  actualizarState = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
   render() {
-    return <div>Nuevo Producto</div>;
+    return (
+      <>
+        <h1 className="text-center">Nuevo Producto</h1>
+        <div className="row justify-content-center">
+          <form className="col-md-8">
+            <div className="form-group">
+              <label>Nombre:</label>
+              <input
+                type="text"
+                name="nombre"
+                className="form-control"
+                placeholder="Nombre del Producto"
+                onChange={this.actualizarState}
+              />
+            </div>
+            <div className="form-group">
+              <label>Precio:</label>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">$</div>
+                </div>
+                <input
+                  type="number"
+                  name="precio"
+                  className="form-control"
+                  placeholder="Precio del Producto"
+                  onChange={this.actualizarState}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Stock:</label>
+              <input
+                type="number"
+                name="stock"
+                className="form-control"
+                placeholder="stock del Producto"
+                onChange={this.actualizarState}
+              />
+            </div>
+            <button type="submit" className="btn btn-success float-right">
+              Crear Producto
+            </button>
+          </form>
+        </div>
+      </>
+    );
   }
 }
 
