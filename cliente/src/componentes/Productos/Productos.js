195b5772a76485c25fe38d5504d61f9f3ba044a2
Cesar Murillo <namiayumis@gmail.com>
2020-01-17 22:58:06 +0900

    need check

--- cliente/src/componentes/Productos/Productos.js
+++ cliente/src/componentes/Productos/Productos.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
 
 export class Productos extends Component {
   render() {
-    return <h1 className="text-center mb-5">Productos</h1>;
+    {
+      ({ loading, error, data, startPolling, stopPolling }) => {
+        if (loading) return "Cargando...";
+        if (error) return `Error: ${error.messages}`;
+        console.log(data);
+
+        return (
+          <>
+            <h1 className="text-center mb-5">Productos</h1>
+          </>
+        );
+      };
+    }
   }
 }
 
