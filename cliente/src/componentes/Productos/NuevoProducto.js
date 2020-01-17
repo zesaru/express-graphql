b75d42c50c3bfc5e19f22983b33cfb185ee173cc
Cesar Murillo <namiayumis@gmail.com>
2020-01-17 16:04:56 +0900

    Incorporando el Mutation de Nuevos Productos

--- cliente/src/componentes/Productos/NuevoProducto.js
+++ cliente/src/componentes/Productos/NuevoProducto.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { NUEVO_PRODUCTO } from "../../mutations";
+import { Mutation } from "react-apollo";
 
 class NuevoProducto extends Component {
   state = {
@@ -17,55 +19,81 @@ class NuevoProducto extends Component {
     const noValido = !nombre || !precio || !stock;
     return noValido;
   };
+
+  crearNuevoProducto = (e, nuevoProducto) => {
+    e.preventDefault();
+
+    //insertamos en la base de datos
+    nuevoProducto().then(data => {
+      console.log(data);
+    });
+  };
+
   render() {
+    const { nombre, precio, stock } = this.state;
+    const input = {
+      nombre,
+      precio: Number(precio),
+      stock: Number(stock)
+    };
+
     return (
       <>
         <h1 className="text-center">Nuevo Producto</h1>
         <div className="row justify-content-center">
-          <form className="col-md-8">
-            <div className="form-group">
-              <label>Nombre:</label>
-              <input
-                type="text"
-                name="nombre"
-                className="form-control"
-                placeholder="Nombre del Producto"
-                onChange={this.actualizarState}
-              />
-            </div>
-            <div className="form-group">
-              <label>Precio:</label>
-              <div className="input-group">
-                <div className="input-group-prepend">
-                  <div className="input-group-text">$</div>
-                </div>
-                <input
-                  type="number"
-                  name="precio"
-                  className="form-control"
-                  placeholder="Precio del Producto"
-                  onChange={this.actualizarState}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label>Stock:</label>
-              <input
-                type="number"
-                name="stock"
-                className="form-control"
-                placeholder="stock del Producto"
-                onChange={this.actualizarState}
-              />
-            </div>
-            <button
-              disabled={this.validarForm()}
-              type="submit"
-              className="btn btn-success float-right"
-            >
-              Crear Producto
-            </button>
-          </form>
+          <Mutation mutation={NUEVO_PRODUCTO} variables={{ input }}>
+            {(nuevoProducto, { loading, error, data }) => {
+              return (
+                <form
+                  className="col-md-8"
+                  onSubmit={e => this.crearNuevoProducto(e, nuevoProducto)}
+                >
+                  <div className="form-group">
+                    <label>Nombre:</label>
+                    <input
+                      type="text"
+                      name="nombre"
+                      className="form-control"
+                      placeholder="Nombre del Producto"
+                      onChange={this.actualizarState}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Precio:</label>
+                    <div className="input-group">
+                      <div className="input-group-prepend">
+                        <div className="input-group-text">$</div>
+                      </div>
+                      <input
+                        type="number"
+                        name="precio"
+                        className="form-control"
+                        placeholder="Precio del Producto"
+                        onChange={this.actualizarState}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label>Stock:</label>
+                    <input
+                      type="number"
+                      name="stock"
+                      className="form-control"
+                      placeholder="stock del Producto"
+                      onChange={this.actualizarState}
+                    />
+                  </div>
+                  <button
+                    disabled={this.validarForm()}
+                    type="submit"
+                    className="btn btn-success float-right"
+                  >
+                    Crear Producto
+                  </button>
+                </form>
+              );
+            }}
+          </Mutation>
         </div>
       </>
     );
--- cliente/src/mutations/index.js
+++ cliente/src/mutations/index.js
@@ -30,3 +30,12 @@ export const ELIMINAR_CLIENTE = gql`
     eliminarCliente(id: $id)
   }
 `;
+
+export const NUEVO_PRODUCTO = gql`
+  mutation nuevoProducto($input: ProductoInput) {
+    nuevoProducto(input: $input) {
+      id
+      nombre
+    }
+  }
+`;
