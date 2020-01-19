0fe2652a086f3525a6936ac48571419c5d0585f3
Cesar Murillo <namiayumis@gmail.com>
2020-01-19 18:28:29 +0900

    Pasando los Datos del Producto a Editar al Formulario

--- cliente/src/componentes/Productos/FormularioEditarProducto.js
+++ cliente/src/componentes/Productos/FormularioEditarProducto.js
@@ -1,8 +1,90 @@
 import React, { Component } from "react";
 
+const initialState = {
+  nombre: "",
+  precio: "",
+  stock: ""
+};
+
 export class FormularioEditar extends Component {
+  state = {
+    ...this.props.producto.obtenerProducto
+  };
+  limpiarState = () => {
+    this.setState({
+      ...initialState
+    });
+  };
+
+  actualizarState = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  validarForm = () => {
+    const { nombre, precio, stock } = this.state;
+    const noValido = !nombre || !precio || !stock;
+    return noValido;
+  };
+
   render() {
-    return <div></div>;
+    const { nombre, precio, stock } = this.state;
+    const input = {
+      nombre,
+      precio: Number(precio),
+      stock: Number(stock)
+    };
+    return (
+      <form className="col-md-8">
+        <div className="form-group">
+          <label>Nombre:</label>
+          <input
+            onChange={this.actualizarState}
+            type="text"
+            name="nombre"
+            className="form-control"
+            placeholder="Nombre del Producto"
+            value={nombre}
+          />
+        </div>
+        <div className="form-group">
+          <label>Precio:</label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text">$</div>
+            </div>
+            <input
+              onChange={this.actualizarState}
+              type="number"
+              name="precio"
+              className="form-control"
+              placeholder="Precio del Producto"
+              value={precio}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Stock:</label>
+          <input
+            onChange={this.actualizarState}
+            type="number"
+            name="stock"
+            className="form-control"
+            placeholder="stock del Producto"
+            value={stock}
+          />
+        </div>
+        <button
+          disabled={this.validarForm()}
+          type="submit"
+          className="btn btn-success float-right"
+        >
+          Guardar Cambios
+        </button>
+      </form>
+    );
   }
 }
 
