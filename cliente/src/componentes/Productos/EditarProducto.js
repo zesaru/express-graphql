5c721011483c1970175e8dcfe30d833901ac5c46
Cesar Murillo <namiayumis@gmail.com>
2020-01-19 18:06:14 +0900

    Obteniendo el ID del Producto a Editar

--- cliente/src/componentes/Productos/EditarProducto.js
+++ cliente/src/componentes/Productos/EditarProducto.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
+import { OBTENER_PRODUCTO } from "../../queries";
+import FormularioEditar from "./FormularioEditarProducto";
 
 class EditarProducto extends Component {
+  state = {};
+
   render() {
-    return <div>Editar Producto</div>;
+    const { id } = this.props.match.params;
+    console.log(id);
+
+    return (
+      <>
+        <h1 className="text-center">Editar Producto</h1>
+        <div className="row justify-content-center">
+          <Query query={OBTENER_PRODUCTO} variables={{ id }}>
+            {({ loading, error, data, refetch }) => {
+              if (loading) return "Cargando...";
+              if (error) return `Error! ${error.message}`;
+              //console.log(data);
+              return (
+                <FormularioEditar producto={data} id={id} refetch={refetch} />
+              );
+            }}
+          </Query>
+        </div>
+      </>
+    );
   }
 }
 
