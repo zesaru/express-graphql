5bd09cc34dd797abdd3a56496c883d78c14a0250
Cesar Murillo <namiayumis@gmail.com>
2020-01-22 17:32:11 +0900

    Agregando los Componentes para los Pedidos

--- cliente/src/componentes/Pedidos/NuevoPedido.js
+++ cliente/src/componentes/Pedidos/NuevoPedido.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import DatosCliente from "./DatosCliente";
 
 export class NuevoPedido extends Component {
   state = {};
   render() {
-    return <></>;
+    const { id } = this.props.match.params;
+    return (
+      <>
+        <h1 className="text-center mb-5">Nuevo Pedido</h1>
+        <div className="row">
+          <div className="col-md-3">
+            <DatosCliente id={id} />
+          </div>
+          <div className="col-md-9">Pedido aqui</div>
+        </div>
+      </>
+    );
   }
 }
 
