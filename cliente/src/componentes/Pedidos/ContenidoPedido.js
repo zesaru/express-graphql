01adbe367b7bf716661a66b540f528656d4ff7f2
Cesar Murillo <namiayumis@gmail.com>
2020-01-23 13:54:02 +0900

    Agregando los Productos Seleccionados al State

--- cliente/src/componentes/Pedidos/ContenidoPedido.js
+++ cliente/src/componentes/Pedidos/ContenidoPedido.js
@@ -3,17 +3,26 @@ import Select from "react-select";
 import makeAnimated from "react-select/animated";
 
 export class ContenidoPedido extends Component {
-  state = {};
+  state = { productos: [] };
+  seleccionarProductos = productos => {
+    this.setState({
+      productos
+    });
+  };
   render() {
     return (
-      <Select
-        options={this.props.productos}
-        isMulti={true}
-        components={makeAnimated()}
-        placeholder={"Seleccionar Producto"}
-        getOptionValue={options => options.id}
-        getOptionLabel={options => options.nombre}
-      />
+      <>
+        <h2 className="text-center mb-5">Seleccionar Productos</h2>
+        <Select
+          onChange={this.seleccionarProductos}
+          options={this.props.productos}
+          isMulti={true}
+          components={makeAnimated()}
+          placeholder={"Seleccionar Producto"}
+          getOptionValue={options => options.id}
+          getOptionLabel={options => options.nombre}
+        />
+      </>
     );
   }
 }
